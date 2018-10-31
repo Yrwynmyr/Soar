c361b8996b6ea09e4b7d94dd2d355bb2c34230ba
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2018-10-31 00:25:30 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="Header">
+      <h1>Garrett Johnson</h1>
+      <h2>Profolio</h2>
+  </div>
+<iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/c871ea8e-b785-4002-a948-6192220f5935">
+</iframe>
+
+      
+      </div>
+    );
+  }
+}
+
+export default App;
