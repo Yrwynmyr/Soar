abe05e1d50a9320fafab3e07e9e908e86b70944e
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2018-10-31 23:08:43 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,12 +28,9 @@ class App extends Component {
       <h1>Garrett Johnson</h1>
       <h2>Profolio</h2>
   </div>
-      <div className="Sidescroll">
-      <div className="row">
-      <p>My detailed portfolio of Machine Learning Notebook and more.</p>
-      </div>
-      
-      </div>
+      <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open tab</span>
+        <p>My detailed portfolio of Machine Learning Notebook and more.</p>
+     
 <iframe
     allow="microphone;"
     width="350"
