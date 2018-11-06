65ad5720143b71ca4d442ec26da4543f22c12d7a
Yrwynmyr <42325411+Yrwynmyr@users.noreply.github.com>
2018-11-06 11:53:49 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,14 +24,13 @@ class App extends Component {
     height="430"
     src="https://console.dialogflow.com/api-client/demo/embedded/c871ea8e-b785-4002-a948-6192220f5935">
 </iframe>
-   <footer>
-   <audio height"300" width="400" controls>
-  <source src={TheHeights} type="audio/mp3"/>
-  <source src={TheHeights} type="audio/mp3"/>
+<div className="Audio">
+   <audio controls>
+  <source src={TheHeights} type="audio/mpeg"/>
+  <source src={TheHeights} type="audio/mpeg"/>
   Your browser does not support HTML5 video.
 </audio>
-
-</footer>
+</div>
       </div>
     );
   }
